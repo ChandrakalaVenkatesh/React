1d52058ab360a56adcd94fa8cd45cd30acd12f4b
ChandrakalaVenkatesh <120547355+ChandrakalaVenkatesh@users.noreply.github.com>
2023-03-29 12:11:17 +0530

    Add files via upload

---
+++ Menu.jsx
@@ -0,0 +1,23 @@
+import React from "react"
+
+const Menu=()=>{
+    return(
+        <div>
+            <ul>
+                <li>
+                    <a href="">HOME</a>
+                </li>
+                <li>
+                    <a href="">LOGIN</a>
+                </li>
+                <li>
+                    <a href="">SIGNUP</a>
+                </li>
+                <li>
+                    <a href="">About</a>
+                </li>
+            </ul>
+        </div>
+    )
+}
+export default Menu;
