b62b39e7e75b8674dc17bda6b13888fe584c62c9
ChandrakalaVenkatesh <120547355+ChandrakalaVenkatesh@users.noreply.github.com>
2023-03-29 12:22:44 +0530

    Add files via upload

---
+++ Netflix.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+const Netflix=()=>{
+
+    return(
+        <>
+        <div className="mainblock"> 
+            <Nav className="navblock">
+                <div>
+                    <img src="" alt="" />
+                </div>
+                <div className="contentblock">
+                    <ul>
+                        <li>
+                           <h3> <a href="">MOVIES</a></h3>
+
+                        </li>
+                        <li>
+                           <h3> <a href="">SERIES</a> </h3>
+                        </li>
+                        <li>
+                           <h3> <a href="">Categories</a> </h3>
+                        </li>
+                    </ul>
+                </div>
+            </Nav>
+        </div>
+        </>
+    )
+}
+export default Netflix;
